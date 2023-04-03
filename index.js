66355f2882ee5c5f219d0189e3215eec5935fcbf
Rodrigo Morán <67351706+Rom1893@users.noreply.github.com>
2023-04-03 16:00:33 -0400

    createReadStream

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 import { parse } from 'csv-parse';
+import fs from 'fs';
 
+const results = [];
+
+fs.createReadStream("kepler_data.csv")
+    .pipe(parse({
+        comment: "#",
+        columns: true,
+    }))
+    .on("data", (data) => {
+        results.push(data)
+    })
+    .on("end", () => {
+        console.log(results)
+        console.log("Done")
+    })
+    .on("error", (err)=>{
+        console.log(err)
+    })
+    ;
